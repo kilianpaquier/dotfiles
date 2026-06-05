b25595531dcbc0fde2586e669f6c6d8ac76368b0
kilianpaquier <git@kilianpaquier.dev>
2026-06-05 22:35:47 +0200

    feat(link): add mise configuration to dotfiles
    Signed-off-by: kilianpaquier <git@kilianpaquier.dev>

--- link.sh
+++ link.sh
@@ -4,6 +4,10 @@ set -e
 
 dir="$(realpath "$(dirname "$0")")"
 
-files=".bash_aliases .bash_logout .bashrc .profile .zshrc"
-for file in $files; do ln -sf "$dir/$file" "$HOME/$file"; done
+files=".bash_aliases .bash_logout .bashrc .profile .zshrc .config/mise/config.toml"
+for file in $files; do
+  link_dir=$(dirname "$file")
+  [ "$link_dir" = "." ] || mkdir -p "$HOME/$link_dir"
+  ln -sf "$dir/$file" "$HOME/$file"
+done
 unset file files
